1b9ec2b3f713d65474ccafb620dc0e7ae3ecb062
BG6MGD <bg6mgd@qq.com>
2024-08-04 16:50:20 +0800

    更新 ts.py

--- ts.py
+++ ts.py
@@ -2,7 +2,7 @@ import requests
 from wxpusher import WxPusher # https://github.com/wxpusher/wxpusher-sdk-python
 
 app_token = 'AT_vK5oR3rY68dsfTkWansUjbERriYZcILs'   # 本处改成自己的应用 APP_TOKEN
-uid_myself = 'UID_GRn5qjZj2AZuBzhdbUYcUUdRfHyf'  # 本处改成自己的 UID
+uid_myself = 'UID_zmh4Ld7lG0qiPSboBPhgQXDg1TA8'  # 本处改成自己的 UID
 def wxpusher_send(msg):
     """利用 wxpusher 的 python SDK ，实现微信信息的发送"""
     result = WxPusher.send_message(msg,
